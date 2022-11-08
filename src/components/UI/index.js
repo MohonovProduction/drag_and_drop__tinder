07494658fd4a742d92e911f98cc95b167533a4e2
Mihail <mohonovproduction@yandex.ru>
2022-11-08 11:52:53 +0300

    build

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,11 +1,9 @@
 import MButton from './MButton'
 import MCard from './MCard'
 import MModal from './MModal'
-import MDropZone from './MDropZone';
 
 export default [
     MButton,
     MCard,
     MModal,
-    MDropZone
 ]
